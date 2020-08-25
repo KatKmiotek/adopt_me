08fdb871f4acd10706a1aa9aca929f1ea7d516dc
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-08-25 15:23:53 +0100

    prettier and eslint added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Pet from './components/Pet'
-import './App.css';
+import React from "react";
+import Pet from "./components/Pet";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Pet = ({name, animal, breed}) => {
-    return (
+const Pet = ({ name, animal, breed }) => {
+  return (
     <div>
-        <h1>{name}</h1>
-        <h2>{animal}</h2>
-        <h2>{breed}</h2>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
     </div>
-    )
-}
+  );
+};
 export default Pet;
