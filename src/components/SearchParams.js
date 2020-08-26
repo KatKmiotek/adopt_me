dd5e6c3e415d4fdd5b4be845971b9c63db9c8244
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-08-26 16:23:51 +0100

    :bug:

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -6,7 +6,7 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
   useEffect(() => {
     setBreeds([]);
