36d37d2c37b496832419c905e557a76ac7d8393c
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-08-26 08:04:10 +0100

    eslint

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import Pet from "./components/Pet";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Misia" animal="Dog" breed="fox"></Pet>
-      <Pet name="Karob" animal="Dog" breed="squirel"></Pet>
-      <Pet name="Sasza" animal="Cat" breed="mixed"></Pet>
+      <Pet name="Misia" animal="Dog" breed="fox" />
+      <Pet name="Karob" animal="Dog" breed="squirel" />
+      <Pet name="Sasza" animal="Cat" breed="mixed" />
     </div>
   );
-}
+};
 
 export default App;
