c9587eeed15610dad7af473529dc3c33836f149f
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-08-26 13:10:43 +0100

    dropdown component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Pet from "./components/Pet";
+import SearchParams from "./components/SearchParams";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Misia" animal="Dog" breed="fox" />
-      <Pet name="Karob" animal="Dog" breed="squirel" />
-      <Pet name="Sasza" animal="Cat" breed="mixed" />
+      <SearchParams></SearchParams>
     </div>
   );
 };
